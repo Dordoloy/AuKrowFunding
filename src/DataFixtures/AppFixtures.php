912cb00c05770f4d0936ac845e81e2efc39bbf68
dordoloy <yohan.dordolo@vrtice.com>
2020-03-15 20:59:23 +0100

    AppFixtures -> categories and status

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,44 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Status;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $allCategories = ["Art"=>[], "Arts and crafts"=>[], "BD"=>[], "Cinema and video"=>[], "Dance"=>[], "Design"=>[],
+            "Editing"=>[], "gastronomy"=>[], "Games"=>["Video game","Board game"], "Journalism"=>[], "Mode"=>[],
+            "Music"=>[], "Photography"=>[], "Technology"=>[], "Theater"=>[]];
+
+        $allStatus = ["Currently funding", "Funded", "Ended"];
+
+        foreach ($allCategories as $category => $subCategories) {
+            $newCategory = new Category();
+            $newCategory->setName($category);
+            $manager->persist($newCategory);
+
+            if(isset($subCategories[0])) {
+                foreach ($subCategories as $subCategory) {
+                    $newSubCategory = new Category();
+                    $newSubCategory->setName($subCategory);
+                    $manager->persist($newSubCategory);
+
+                    $newCategory->addCategory($newSubCategory);
+                }
+            }
+        }
+
+        foreach ($allStatus as $status) {
+            $newStatus = new Status();
+            $newStatus->setName($status);
+            $manager->persist($newStatus);
+        }
+
 
         $manager->flush();
     }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -129,4 +129,9 @@ class Category
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return  $this->getName();
+    }
 }
