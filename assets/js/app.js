8fa323be1dfe6ad67c72cb2a9b4c8443412bd397
anthonychaussin <chaussin.anthony@gmail.com>
2020-03-18 13:07:34 +0100

    User profile

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import '../css/profile.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
