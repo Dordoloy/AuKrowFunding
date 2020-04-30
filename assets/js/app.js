404a374e8d5fd4172d2e6a19bb7c994efd24da76
dordoloy <yohan.dordolo@vrtice.com>
2020-04-30 23:16:35 +0200

    - Some style

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import '../css/app.scss';
 import '../css/profile.css';
 import '../js/tools'
 import '../css/project.css';
